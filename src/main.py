1226ab1b2ea7c99be6e33a97e702aab723b2efa1
Ehsan Taheri <ehsan.taheri1400@gmail.com>
2025-04-11 17:10:10 +0200

    Update

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from config_reader import read_config
 from data_ingestion import DataIngestion
 from data_preprocessing import DataPreprocessing
+from model_training import ModelTraining
 
 data_inges=DataIngestion(read_config("config/config.yaml"))
 data_inges.run()
 
 data_preprocessing=DataPreprocessing(read_config("config/config.yaml"))
 data_preprocessing.run()
+
+model_training=ModelTraining(read_config("config/config.yaml"))
+model_training.run()
+
